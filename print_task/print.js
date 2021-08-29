2332f317e9e5319889c983522dc4f8ef22263f9e
matveyprupas <matveyprupas@gmail.com>
2021-08-29 11:52:57 +0300

    fix: print task done 30/30

--- print_task/print.js
+++ print_task/print.js
@@ -6,6 +6,7 @@ function print(str) {
     .map(el => +el)
     .sort( (a,b)=>a-b )
     .filter( (el, i, arr) => el !== arr[i-1] );
+    // console.log(result);
 
     for (let i = 0; i < result.length; i++) {
         if (result[i] - 1 !== result[i-1] && result[i] + 1 === result[i+1]) {
@@ -14,14 +15,22 @@ function print(str) {
         }
         if(result[i] + 1 === result[i+1]) continue; 
         if(result[i] - 1 === result[i-1]) {
-            resStr += "-" + result[i];
+            resStr += "-" + result[i] + ",";
             continue;
         }
         resStr += result[i] + ",";
     }
+    resStr = resStr.trim();
+    if (resStr[resStr.length - 1] === ",") {
+        resStr = resStr.slice(0, -1);
+        // console.log(resStr);
+    }
 
-    return resStr.trim();
+    return resStr;
 }
+// console.log(print("1,3,1,4,5"));
+// console.log(print("1,3,1,6,7,8,9,12,125,16,4,5"));
+// console.log(print("1,3,1,4,5"));
 
 const fs = require('fs');
 const input = fs.readFileSync(0, 'utf-8');
